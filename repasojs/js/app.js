695fb4d412842295c574f00ecb37f273519f91d3
HectorAlarconGuerra <33630876+HectorAlarconGuerra@users.noreply.github.com>
2021-01-27 14:31:58 -0500

    Update app.js
    Herencia

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -390,7 +390,21 @@ class Tarea {
       this.prioridad = prioridad
   }
   mostrar(){
-    return(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
+    console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
+  }
+}
+
+class ComprasPendientes extends Tarea{
+  constructor(nombre, prioridad, cantidad){
+     super(nombre, prioridad);
+     this.cantidad = cantidad;
+  }
+  mostrar(){
+    super.mostrar();
+    console.log(`y la cantidad ${this.cantidad}`);
+  }
+  hola(){
+    return 'hola';
   }
 }
 
@@ -400,7 +414,11 @@ let tarea2 = new Tarea('Prepara café', 'Alta')
 let tarea3 = new Tarea('Pasear al perro', 'Media')
 let tarea4 = new Tarea('Conocer a mis suegro', 'Baja')
 
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-console.log(tarea4.mostrar());
+// console.log(tarea1.mostrar());
+// console.log(tarea2.mostrar());
+// console.log(tarea3.mostrar());
+// console.log(tarea4.mostrar());
+
+let compra1 = new ComprasPendientes('Jabon','Urgente', 3)
+compra1.mostrar();
+console.log(compra1.hola())
